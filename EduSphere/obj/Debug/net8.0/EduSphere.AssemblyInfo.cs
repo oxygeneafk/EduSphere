5ebeed319cc56003417003db174c4abfdd60dce2
oxyyylesnar. <79524100+oxygeneafk@users.noreply.github.com>
2025-06-13 00:09:05 +0300

    a

--- EduSphere/obj/Debug/net8.0/EduSphere.AssemblyInfo.cs
+++ EduSphere/obj/Debug/net8.0/EduSphere.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5b3035d163f7b57cd6efb05b69f55ec831cdee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33705ba0fa959167c48f65a051d621c6c0daf5b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
